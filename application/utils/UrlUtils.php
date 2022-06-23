270a6590d4cebf47d7059fb8a72ac5681c0ab355
sbsteacher <52367146+sbsteacher@users.noreply.github.com>
2022-06-23 10:07:12 +0900

    Create UrlUtils.php

---
+++ application/utils/UrlUtils.php
@@ -0,0 +1,17 @@
+<?php
+    function getUrlPaths() {
+        $getUrl = '';
+        if (isset($_GET['url'])) {
+            $getUrl = rtrim($_GET['url'], '/');
+            $getUrl = filter_var($getUrl, FILTER_SANITIZE_URL);
+        }
+        return explode('/', $getUrl);
+    }
+
+    function isGetOne() {
+        $urlPaths = getUrlPaths();
+        if(isset($urlPaths[2])) { //one
+            return $urlPaths[2];
+        }
+        return false;
+    }
