d1afbb8882e0ee1d34880376a50085ca2deeddd2
sbsteacher <52367146+sbsteacher@users.noreply.github.com>
2022-06-23 10:03:30 +0900

    Update Controller.php

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -1,10 +1,21 @@
 <?php
 namespace application\controllers;
 
-abstract class Controller {
-    public function __construct($action) {        
+class Controller {    
+    protected $model;
+
+    public function __construct($action, $model) {    
+        if(!isset($_SESSION)) {
+            session_start();
+        }    
+        $this->model = $model;
         $view = $this->$action();
-        require_once $this->getView($view); 
+        if(gettype($view) === "string") {
+            require_once $this->getView($view);             
+        } else if(gettype($view) === "object" || gettype($view) === "array") {
+            header("Content-Type:application/json");
+            echo json_encode($view);
+        }        
     }
     
     protected function addAttribute($key, $val) {
@@ -14,9 +25,19 @@ abstract class Controller {
     protected function getView($view) {
         if(strpos($view, "redirect:") === 0) {
             header("Location: " . substr($view, 9));
-            return;
+            exit();
         }
-        return _VIEW . "/" . $view;
+        return _VIEW . $view;
+    }
+
+    protected function flash($name = '', $val = '') {
+        if(!empty($name)) { //공백이 아니면
+            if(!empty($val)) {
+                $_SESSION[$name] = $val;
+            } else if(empty($val) && !empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
+            }
+        }
     }
 }
 
