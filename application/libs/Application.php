434eae5babead18ae0efbde62eba0ae5e5674b26
sbsteacher <52367146+sbsteacher@users.noreply.github.com>
2022-06-23 10:04:23 +0900

    Update Application.php

--- application/libs/Application.php
+++ application/libs/Application.php
@@ -1,26 +1,31 @@
 <?php
 namespace application\libs;
 
+require_once "application/utils/UrlUtils.php";
+
 class Application{
     
     public $controller;
     public $action;
+    private static $modelList = [];
 
-    public function __construct() {
-        $getUrl = '';
-        if (isset($_GET['url'])) {
-            $getUrl = rtrim($_GET['url'], '/');
-            $getUrl = filter_var($getUrl, FILTER_SANITIZE_URL);
-        }        
-        $getParams = explode('/', $getUrl);
-        $controller = isset($getParams[0]) && $getParams[0] != '' ? $getParams[0] : 'board';
-        $action = isset($getParams[1]) && $getParams[1] != '' ? $getParams[1] : 'index';
+    public function __construct() {        
+        $urlPaths = getUrlPaths();
+        $controller = isset($urlPaths[0]) && $urlPaths[0] != '' ? $urlPaths[0] : 'board';
+        $action = isset($urlPaths[1]) && $urlPaths[1] != '' ? $urlPaths[1] : 'index';
 
         if (!file_exists('application/controllers/'. $controller .'Controller.php')) {
             echo "해당 컨트롤러가 존재하지 않습니다.";
             exit();
         }
-        $controllerName = 'application\controllers\\' . $controller . 'controller';        
-        new $controllerName($action);
+
+        if(!in_array($controller, static::$modelList)) {
+            $modelName = 'application\models\\' . $controller . 'model';
+            static::$modelList[$controller] = new $modelName();
+        }
+
+        $controllerName = 'application\controllers\\' . $controller . 'controller';                
+        $model = static::$modelList[$controller];
+        new $controllerName($action, $model);
     }
 }
