0dc7e03c586249329d6e7a8c330610231974aa39
sbsteacher <52367146+sbsteacher@users.noreply.github.com>
2022-06-24 17:49:35 +0900

    Update UrlUtils.php

--- application/utils/UrlUtils.php
+++ application/utils/UrlUtils.php
@@ -1,15 +1,17 @@
 <?php
+    function getParam($key) {
+        return isset($_GET[$key]) ? $_GET[$key] : "";
+    }
+    function getUrl() {
+        return isset($_GET['url']) ? rtrim($_GET['url'], '/') : "";
+    }
     function getUrlPaths() {
-        $getUrl = '';
-        if (isset($_GET['url'])) {
-            $getUrl = rtrim($_GET['url'], '/');
-            $getUrl = filter_var($getUrl, FILTER_SANITIZE_URL);
-        }
-        return explode('/', $getUrl);
+        $getUrl = getUrl();        
+        return $getUrl !== "" ? explode('/', $getUrl) : "";
     }
 
-    function getMethod() {
-       return $_SERVER['REQUEST_METHOD'];
+    function getMethod() {        
+        return $_SERVER['REQUEST_METHOD'];
     }
 
     function isGetOne() {
