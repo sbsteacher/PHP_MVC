f55782d03f3b5510f21f5bf976c6ba941a87e5fb
sbsteacher <52367146+sbsteacher@users.noreply.github.com>
2022-07-12 10:03:41 +0900

    Update UrlUtils.php

--- application/utils/UrlUtils.php
+++ application/utils/UrlUtils.php
@@ -1,10 +1,16 @@
 <?php
+    function getJson() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+    
     function getParam($key) {
         return isset($_GET[$key]) ? $_GET[$key] : "";
     }
+
     function getUrl() {
         return isset($_GET['url']) ? rtrim($_GET['url'], '/') : "";
     }
+
     function getUrlPaths() {
         $getUrl = getUrl();        
         return $getUrl !== "" ? explode('/', $getUrl) : "";
